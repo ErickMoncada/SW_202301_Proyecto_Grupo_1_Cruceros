fd8461e0f09a78f76c8e631fe0310f95f34a252f
lesninramirez <79343379+lesninramirez@users.noreply.github.com>
2023-04-18 00:35:09 -0600

    Cambio Hora

--- src/dao/models/Destinos/IDestinos.ts
+++ src/dao/models/Destinos/IDestinos.ts
@@ -9,13 +9,13 @@ export interface IDestinos extends IAuditable {
   _id?: ObjectId | string;
   pais: string;
   status: string;
-  fechaComienzo: Date;
+  fechaComienzo: string;
 }
 export const DefaultDestinos:IDestinos={
   pais: "",
   status: "",
   createdAt: new Date(),
   updatedAt: new Date(),
-  fechaComienzo: undefined
+  fechaComienzo: ""
 }
 
